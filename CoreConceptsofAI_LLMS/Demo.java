ed10d8b51048f9adb94f045d622ecd9e42894a52
Sai Gopinath <sainath.gopinath@gmail.com>
2025-11-12 21:24:50 -0500

    AIEngg: AI notes + demo notes

---
+++ CoreConceptsofAI_LLMS/Demo.java
@@ -0,0 +1,22 @@
+/// we can write a lot of code.
+// we inject something here for LLM
+/// we can use OpenAI, Gemini, Claude
+// However, cloud services are not free
+// first i need to login into OpenAI: https://auth.openai.com/log-in
+// then i can use the API key to call the service
+// https://platform.openai.com/settings/organization/api-keys
+// Create new secret key
+// Either use API key to call OpenAI models or download models in local
+// Go to Ollama and download models
+// Use Gemma3 or deepseek-r1
+// open Powershell and type ollama run deepseek-r1:1.5b
+
+
+
+
+
+
+
+
+
+
