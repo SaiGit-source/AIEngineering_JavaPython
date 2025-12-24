3892436402b023633b44746f3604b0002816f077
Sai Gopinath <sainath.gopinath@gmail.com>
2025-12-23 22:00:18 -0500

    AIEngg: MCP_Client demo added

--- MCP/mcp_demo/mcp_demo/src/main/java/com/SpringAI/mcp_demo/controller/MCPController.java
+++ MCP/mcp_demo/mcp_demo/src/main/java/com/SpringAI/mcp_demo/controller/MCPController.java
@@ -1,6 +1,7 @@
 package com.SpringAI.mcp_demo.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,8 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class MCPController {
     private ChatClient chatClient;
-    public MCPController(ChatClient.Builder chatClientBuilder){
-        this.chatClient = chatClientBuilder.build();
+
+    // we need to inform ChatClient that we are calling MCP Server, so add ToolCallBaclProvider
+    public MCPController(ChatClient.Builder chatClientBuilder, ToolCallbackProvider toolCallBackProvider){
+        this.chatClient = chatClientBuilder
+                .defaultToolCallbacks(toolCallBackProvider)
+                .build();
     }
     @GetMapping("/chat")
     public String getAnswer(@RequestParam String question)
@@ -24,4 +29,18 @@ public class MCPController {
 // Output
 /*
 Java is a high-level, object-oriented programming language that was originally developed by Sun Microsystems, which is now part of Oracle Corporation. It was first released in 1995 and has since become one of the most widely used programming languages in the world.
+ */
+
+// GET http://localhost:8080/api/chat?question=Create a file test.txt with content "MCP-servers test is successful" in the 'mcp_server' folder
+/*
+The file `test.txt` has been successfully created in the `mcp_server` folder with the content "MCP-servers test is successful."
+ */
+
+// GET http://localhost:8080/api/chat?question=List the files in the mcp_servers folder and its content
+
+/*
+The files in the `mcp_servers` folder are as follows:
+
+- **test.txt**
+  - Content: "MCP-servers test is successful"
  */
