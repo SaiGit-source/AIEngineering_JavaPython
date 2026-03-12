34a53d996c804cdccfda280d4a896431cbf2c152
Sai Gopinath <sainath.gopinath@gmail.com>
2026-03-11 21:37:21 -0400

    AIEngg: Ecomm SpringAI added

---
+++ Project/Ecommerce_SpringAI/Ecomm-backend/src/main/java/com/springAI/Ecommerce/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.springAI.Ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
