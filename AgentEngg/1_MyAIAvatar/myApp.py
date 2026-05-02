a07232f70b7cfb0288a73ce00b40bd1f60763049
Sai Gopinath <sainath.gopinath@gmail.com>
2026-05-02 16:07:24 -0400

    AIEngg: AgentEngg_MyAvatar

---
+++ AgentEngg/1_MyAIAvatar/myApp.py
@@ -0,0 +1,151 @@
+### To run: uv run .\myApp.py
+
+# imports
+
+from dotenv import load_dotenv
+from openai import OpenAI
+import json
+import os
+import requests
+from pypdf import PdfReader
+import gradio as gr
+
+# The usual start
+
+load_dotenv(override=True)
+openai = OpenAI()
+google_api_key = os.getenv('GOOGLE_API_KEY')
+
+def record_user_details(email, name="Name not provided", notes="not provided"):
+    print(f"Recording interest from {name} with email {email} and notes {notes}")
+    return {"recorded": "ok"}
+
+def record_unknown_question(question):
+    print(f"Recording {question} asked that I couldn't answer")
+    return {"recorded": "ok"}
+
+
+## These JSONs are available globally to LLMs and tells them when to use these tools and how to format the input to these tools. The LLM will decide when to call these functions based on the conversation, and will use the descriptions and parameters defined here to know how to call them correctly.
+record_user_details_json = {
+    "name": "record_user_details",
+    "description": "Use this tool to record that a user is interested in being in touch and provided an email address",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "email": {
+                "type": "string",
+                "description": "The email address of this user"
+            },
+            "name": {
+                "type": "string",
+                "description": "The user's name, if they provided it"
+            }
+            ,
+            "notes": {
+                "type": "string",
+                "description": "Any additional information about the conversation that's worth recording to give context"
+            }
+        },
+        "required": ["email"],
+        "additionalProperties": False
+    }
+}
+
+
+record_unknown_question_json = {
+    "name": "record_unknown_question",
+    "description": "Always use this tool to record any question that couldn't be answered as you didn't know the answer",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "question": {
+                "type": "string",
+                "description": "The question that couldn't be answered"
+            },
+        },
+        "required": ["question"],
+        "additionalProperties": False
+    }
+}
+
+
+tools = [{"type": "function", "function": record_user_details_json},
+        {"type": "function", "function": record_unknown_question_json}]
+
+# This function can take a list of tool calls, and run them. This is the IF statement!!
+
+class Me:
+
+    def __init__(self):
+        self.openai = OpenAI()
+        self.name = "Sai Gopinath"
+        reader = PdfReader("sai/linkedin.pdf")
+        self.linkedin = ""
+        for page in reader.pages:
+            text = page.extract_text()
+            if text:
+                self.linkedin += text
+        with open("sai/summary.txt", "r", encoding="utf-8") as f:
+            self.summary = f.read()
+
+
+    def handle_tool_call(self, tool_calls):
+        results = []
+        for tool_call in tool_calls:
+            tool_name = tool_call.function.name
+            arguments = json.loads(tool_call.function.arguments)
+            print(f"Tool called: {tool_name}", flush=True)
+            tool = globals().get(tool_name)
+            result = tool(**arguments) if tool else {}
+            results.append({"role": "tool","content": json.dumps(result),"tool_call_id": tool_call.id})
+        return results
+    
+    def system_prompt(self):
+        system_prompt = f"You are acting as {self.name}. You are answering questions on {self.name}'s website, \
+        particularly questions related to {self.name}'s career, background, skills and experience. \
+        Your responsibility is to represent {self.name} for interactions on the website as faithfully as possible. \
+        You are given a summary of {self.name}'s background and LinkedIn profile which you can use to answer questions. \
+        Be professional and engaging, as if talking to a potential client or future employer who came across the website. \
+        If you don't know the answer to any question, use your record_unknown_question tool to record the question that you couldn't answer, even if it's about something trivial or unrelated to career. \
+        If the user is engaging in discussion, try to steer them towards getting in touch via email; ask for their email and record it using your record_user_details tool. "
+
+        system_prompt += f"\n\n## Summary:\n{self.summary}\n\n## LinkedIn Profile:\n{self.linkedin}\n\n"
+        system_prompt += f"With this context, please chat with the user, always staying in character as {self.name}."
+        return system_prompt
+
+
+    def chat(self, message, history):
+        messages = [{"role": "system", "content": self.system_prompt()}] + history + [{"role": "user", "content": message}]
+        done = False
+        while not done:
+
+            # This is the call to the LLM - see that we pass in the tools json
+
+            gemini = OpenAI(api_key=google_api_key, base_url="https://generativelanguage.googleapis.com/v1beta/openai/")
+            ## We are using OpenAI library to connect to Google base_url and Google has provided an OpenAI conpatible API with a keyword ending in "openai"
+            model_name = "gemini-2.5-flash"
+
+            response = gemini.chat.completions.create(model=model_name, messages=messages, tools=tools)
+
+
+            finish_reason = response.choices[0].finish_reason
+            
+            # If the LLM wants to call a tool, we do that!
+            
+            if finish_reason=="tool_calls":
+                message = response.choices[0].message
+                tool_calls = message.tool_calls
+                results = self.handle_tool_call(tool_calls)
+                messages.append(message)
+                messages.extend(results)
+            else:
+                done = True
+        return response.choices[0].message.content
+
+
+
+if __name__ == "__main__":
+    me = Me()
+    gr.ChatInterface(me.chat, type="messages").launch()
+    
+    
